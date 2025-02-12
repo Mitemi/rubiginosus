5524baf3bb228c9dc5ad7621a1cffc867f9ba8c3
Mitemi <miter360@gmail.com>
2025-02-12 09:59:20 -0500

    1.0.0

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+require("content/items/items");
+//require("content/blocks/blocks");
+require("content/blocks/productionBlocks");
+//require("content/blocks/powerBlocks");
+//require("content/blocks/turretBlocks");
+require("content/misc/vanilla");
+require("content/misc/bullets");
+
+require("content/tech/contentLoader");
+require("content/tech/tech");
+
+require("ui/ui");
+require("autoUpdater");
